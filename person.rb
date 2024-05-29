aeba1fccfe5d97631ff44c1048ab6d444ea41e25
juribe <jse.uribe@gmail.com>
2024-05-29 09:05:40 -0500

    upload documentation

---
+++ person.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# This is a class for person
+class Person
+  # makes the person walks
+  # @return [String] Im walking
+  def walk
+    puts 'Im walking'
+  end
+
+  # makes the person talks
+  # @return [String] Im talking
+  def talk
+    puts 'Im talking'
+  end
+end
